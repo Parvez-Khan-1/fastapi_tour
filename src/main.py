90f325ae9f6bfdf3644c6f1ef751568b2cbb4922
Parvez-Khan-1 <parvezpathan09@gmail.com>
2023-08-13 21:32:29 +0530

    Added status code, tags and description docs

--- src/main.py
+++ src/main.py
@@ -1,18 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 from typing import Optional
 from enum import Enum
 
 app = FastAPI()
-
-
-@app.get("/health")
-def health():
-    return {"message": "FastAPI app is running..."}
-
-
-@app.get("/blog/all")
-def get_all_blogs(page=1, page_size: Optional[int] = None):
-    return {"message": f"All {page} Blogs with page size {page_size}"}
 
 
 class BlogType(str, Enum):
@@ -21,13 +11,42 @@ class BlogType(str, Enum):
     howto = "howto"
 
 
-@app.get("/blog/type/{type}")
+@app.get("/health", tags=["health"])
+def health():
+    return {"message": "FastAPI app is running..."}
+
+
+@app.get("/blog/all",
+         tags=["blog"],
+         summary="Retrieve all blogs",
+         description="This api call simulate retrieve all blogs",
+         response_description="The list of available blogs")
+def get_all_blogs(page=1, page_size: Optional[int] = None):
+    return {"message": f"All {page} Blogs with page size {page_size}"}
+
+
+@app.get("/blog/{id}/comments/{comment_id}/", tags=["blog", "comment"])
+def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
+    """
+    Simulates retrieving comment of a blogs
+    - **id** mandatory parameter
+    - **comment_id** mandatory parameter
+    - **valid** (Optional): bool variable
+    - **username** (optional): str variable
+    """
+    return {"message": f"blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}"}
+
+
+@app.get("/blog/type/{type}", tags=["blog"])
 def get_blog_type(type: BlogType):
     return {"message": f"Returning blog type : {type}"}
 
 
-@app.get('/blog/{id}')
-def get_blog(id: int):
-    return {"message": f"Blog with id {id}"}
-
-
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, tags=['blog'])
+def get_blog(id: int, response: Response):
+    if id > 5:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"error": f"Blog With {id} Not Found"}
+    else:
+        response.status_code = status.HTTP_200_OK
+        return {"message": f"Blog with id {id}"}
