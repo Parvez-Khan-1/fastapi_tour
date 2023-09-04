80c19778531d3543a0628a860937d7fb966ea677
Parvez-Khan-1 <parvezpathan09@gmail.com>
2023-09-04 13:30:42 +0530

    Added get all users endpoint

--- routers/user.py
+++ routers/user.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from schemas import UserBase, UserDisplay
+from typing import List
 from db.database import get_db
 from db import db_user
 
@@ -14,5 +15,8 @@ def create_user(request: UserBase, db: Session = Depends(get_db)):
 
 
 # Read
+@router.get("/user", response_model=List[UserDisplay])
+def get_all_users(db: Session = Depends(get_db)):
+    return db_user.get_all_users(db)
 
 # Update
